0e5fa3d18ad9b96f384d2143c523b0458ade7a1f
Сергей Магомедов <sergejmagomedov@MacBook-Pro-Sergej-Magomedov.local>
2008-02-27 22:18:03 +0300

    init

---
+++ dog.h
@@ -0,0 +1,15 @@
+//
+//  dog.h
+//
+//  Created by Сергей Магомедов on 27.02.08.
+//  Copyright 2008 Модуль. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface dog : /* Specify a superclass (eg: NSObject or NSView) */ {
+
+}
+
+@end
---
+++ dog.m
@@ -0,0 +1,12 @@
+//
+//  dog.m
+//
+//  Created by Сергей Магомедов on 27.02.08.
+//  Copyright 2008 Модуль. All rights reserved.
+//
+
+#import "dog.h"
+
+@implementation dog
+
+@end
